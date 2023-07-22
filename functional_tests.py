04a62e482ddc132e1b041777722be346e9e49570
gr <grim322@bk.ru>
2023-07-22 18:10:35 +0300

    FT checks ability to add list element

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,10 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 service = ChromeService(executable_path=ChromeDriverManager().install())
@@ -22,17 +25,38 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         
         # Она видит, что заголовок и шапка страницы говорят о списках # неотложных дел
-        assert 'To-Do' in self.browser.title
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+        
+        # Ей сразу же предлагается ввести элемент списка
+        
+        input_box = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        # Она набирает в текстовом поле "Купить павлиньи перья" (ее
+        # хобби – # вязание рыболовных мушек)
+        input_box.send_keys('Купить павлиньи перья')
+        # Когда она нажимает enter, страница обновляется, и теперь страница # содержит "1: Купить павлиньи перья" в
+        # качестве элемента списка
+        
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
+        
+        # Текстовое поле по-прежнему приглашает ее добавить еще один элемент. # Она вводит
+        # "Сделать мушку из павлиньих перьев" (Эдит очень методична)
         self.fail('Закончить тест!')
-        
-        # Ей сразу же предлагается ввести элемент списка Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби – #
-        # вязание рыболовных мушек)
-        
-        # Когда она нажимает enter, страница обновляется, и теперь страница # содержит "1: Купить
-        # павлиньи перья" в качестве элемента списка Текстовое поле по-прежнему приглашает ее добавить еще один элемент. #
-        # Она вводит "Сделать мушку из павлиньих перьев" (Эдит очень методична) Страница снова обновляется,
-        # и теперь показывает оба элемента ее списка Эдит интересно, запомнит ли сайт ее список. Далее она видит, что # сайт
-        # сгенерировал для нее уникальный URL-адрес – об этом выводится небольшой текст с объяснениями. Она посещает этот
+        # Страница снова обновляется, и теперь показывает
+        # оба элемента ее списка Эдит интересно, запомнит ли сайт ее список. Далее она видит, что # сайт сгенерировал
+        # для нее уникальный URL-адрес – об этом выводится небольшой текст с объяснениями. Она посещает этот
         # URL-адрес – ее список по-прежнему там. # Удовлетворенная, она снова ложится спать
 
 
