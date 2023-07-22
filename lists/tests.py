ec4a1cd0cbf3046c127dd18db8802fd6090acf7a
gr <grim322@bk.ru>
2023-07-22 17:16:11 +0300

    Base template returns minimal html

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest, HttpResponse
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
@@ -9,4 +10,13 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-        
+    
+    def test_home_page_returns_correct_html(self):
+        """Test home page gives right response"""
+        request = HttpRequest()
+        response: HttpResponse = home_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+    
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home_page(request):
     """Show home page"""
-    pass
+    return HttpResponse('<html>'
+                        '<title>To-Do lists</title>'
+                        '</html>')
